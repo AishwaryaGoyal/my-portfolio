b1f55e33dd72eecc3550af1fc9329f1251292652
Aishwarya Goyal <aishwaryag@crowdstaffing.com>
2021-11-12 14:49:08 +0530

    Carousel auto-animate *not complete*

--- script.js
+++ script.js
@@ -18,3 +18,43 @@ function menuClick() {
     document.getElementById("btnAboutDrop").style.display = "none";
   }
 }
+
+setTimeout(carouselAnimate1, 25000);
+setInterval(carouselAnimate1, 25000);
+
+setTimeout(carouselAnimate2, 5000);
+setInterval(carouselAnimate2, 25000);
+
+setTimeout(carouselAnimate3, 10000);
+setInterval(carouselAnimate3, 25000);
+
+setTimeout(carouselAnimate4, 15000);
+setInterval(carouselAnimate4, 25000);
+
+setTimeout(carouselAnimate5, 20000);
+setInterval(carouselAnimate5, 25000);
+
+function carouselAnimate1() {
+  console.log("clicked1");
+  document.getElementById("radio1").click();
+}
+
+function carouselAnimate2() {
+  console.log("clicked2");
+  document.getElementById("radio2").click();
+}
+
+function carouselAnimate3() {
+  console.log("clicked3");
+  document.getElementById("radio3").click();
+}
+
+function carouselAnimate4() {
+  console.log("clicked4");
+  document.getElementById("radio4").click();
+}
+
+function carouselAnimate5() {
+  console.log("clicked5");
+  document.getElementById("radio5").click();
+}
